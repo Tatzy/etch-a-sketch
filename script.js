c9444b67865050b923bda7672cf40264dfbc78bc
Tatzy <108587201+Tatzy@users.noreply.github.com>
2024-09-10 22:04:38 +0200

    Finish Etch-a-sketch algorithm -Add Interactive Grid -Add Grid-Size-Selector with refreshing off the grid

--- script.js
+++ script.js
@@ -1,22 +1,48 @@
 const container = document.querySelector("#container");
+const button = document.querySelector("#button");
 
+let input = 16;
 let squares = [];
 let row = [];
 
-    //first element [x][0] is for creating a row of 16 squares with flexbox
-    for(let i=0;i<16;i++){
-        row.push(document.createElement("div"));
-        row[i].classList.add("flex");
-        container.appendChild(row[i]);
-    }
-    
+function makeGrid(size){
+    squares = [];
+    row = [];
 
-    
-for(let m=0;m<16;m++){
+//creating a row to fit squares with flexbox
+for (let i = 0; i < size; i++) {
+    row.push(document.createElement("div"));
+    row[i].classList.add("flex");
+    container.appendChild(row[i]);
+}
+
+
+
+for (let m = 0; m < size; m++) {
     squares.push(new Array);
-    for(let n=0;n<16;n++){
+    for (let n = 0; n < size; n++) {
         squares[m].push(document.createElement("div"));
         squares[m].at(n).classList.add("square");
         row[m].appendChild(squares[m].at(n));
+        squares[m].at(n).addEventListener("mouseover", () => {
+            squares[m].at(n).setAttribute("style", "background: red;");
+        })
     }
 }
+}
+
+function deleteGrid(){
+    row.forEach(element=>{element.remove();});
+    squares.forEach(element => {element.forEach(element=>{element.remove();})});
+}
+
+
+makeGrid(input);
+ 
+button.addEventListener("click",()=>{
+
+    deleteGrid();
+    input = prompt("Type in the number of squares per side");
+    makeGrid(input);
+    
+})
